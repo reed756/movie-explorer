a4a4dd1514e5d258e5e3cc52d63b0373f7c1b20e
reedanj <james.r@unitaskr.com>
2024-08-14 17:54:35 +0100

    make header hide on scroll

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, SearchBarComponent, RouterLink, MatIconModule],
+  imports: [MatToolbarModule, SearchBarComponent, RouterLink, MatIconModule, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  scrolled = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event: any) {
+    const numb = window.scrollY;
+    if (numb >= 50) {
+      this.scrolled = 1;
+    }
+    else {
+      this.scrolled = 0;
+    }
+  }
 
   router = inject(Router);
   showSearchBar = false;
