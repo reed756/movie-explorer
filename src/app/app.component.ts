afda30783a5688d91e2aee3104fc3c35ba477aa0
reedanj <james.r@unitaskr.com>
2024-08-16 10:32:40 +0100

    add progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { MovieService } from './shared/services/movie.service';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MatProgressBarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [MovieService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  constructor() {}
-
-  ngOnInit(): void {
-  }
+  movieService = inject(MovieService);
+  isLoading = this.movieService.isLoading;
 
   title = 'movie-explorer';
 }
