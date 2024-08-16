b599805ae0f56bd26a00e553c0e101832b3be48f
reedanj <james.r@unitaskr.com>
2024-08-16 10:40:50 +0100

    add animation to movie details and remove spinner as have progress bar now

--- src/app/modules/movie-details/movie-details.component.ts
+++ src/app/modules/movie-details/movie-details.component.ts
@@ -3,13 +3,20 @@ import { MovieService } from '../../shared/services/movie.service';
 import { AsyncPipe, CurrencyPipe, DatePipe, DecimalPipe, NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
   imports: [AsyncPipe, MatCardModule, NgIf, DatePipe, CurrencyPipe, DecimalPipe, MatProgressSpinnerModule],
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.scss'
+  styleUrl: './movie-details.component.scss',
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [style({ opacity: 0 }), animate('1500ms', style({ opacity: 1 }))]),
+      transition(':leave', [animate('1500ms', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class MovieDetailsComponent {
 
