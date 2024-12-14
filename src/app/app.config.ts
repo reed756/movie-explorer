814293040a5f25944a375b0a20cedf4c3865d178
reedanj <james.r@unitaskr.com>
2024-12-14 15:00:16 +0000

    add transitions api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
@@ -8,7 +7,7 @@ import { IMAGE_CONFIG } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     {
