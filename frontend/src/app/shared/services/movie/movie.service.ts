232c88cafa122c41335c24730d8d466aa774f95d
reedanj <james.r@unitaskr.com>
2025-06-29 12:24:18 +0100

    remove api key from frontend

--- frontend/src/app/shared/services/movie/movie.service.ts
+++ frontend/src/app/shared/services/movie/movie.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, resource, signal } from '@angular/core';
-import { environment } from '../../../../environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +8,10 @@ export class MovieDataClient {
   private options = {
     headers: {
       accept: 'application/json',
-      Authorization: environment.apiKey,
     },
   };
 
-  private apiUrl: string = 'https://api.themoviedb.org/3/';
+  private apiUrl: string = 'http://localhost:3000/api';
   searchTerm = signal<string | null | undefined>('');
   selectedMovieId = signal<number | undefined>(0);
   trendingMovieToggle = signal<string | undefined>('day');
@@ -23,7 +21,7 @@ export class MovieDataClient {
   public searchResults = resource({
     params: () => ({ searchTerm: this.searchTerm() }),
     loader: async ({ params }) => {
-      const response = await fetch(`http://localhost:3000/api/search/${params.searchTerm}`, this.options);
+      const response = await fetch(`${this.apiUrl}/search/${params.searchTerm}`, this.options);
       return response.json();
     }
   })
@@ -31,7 +29,7 @@ export class MovieDataClient {
   public movieSelected = resource({
     params: () => ({ movieId: this.selectedMovieId() }),
     loader: async ({ params }) => {
-      const response = await fetch(`http://localhost:3000/api/movie/${params.movieId}`, this.options);
+      const response = await fetch(`${this.apiUrl}/movie/${params.movieId}`, this.options);
       return response.json();
     }
   })
@@ -39,7 +37,7 @@ export class MovieDataClient {
   public trendingMovies = resource({
     params: () => ({ toggleValue: this.trendingMovieToggle() }),
     loader: async ({ params }) => {
-      const response = await fetch(`http://localhost:3000/api/trending/${params.toggleValue}`, this.options);
+      const response = await fetch(`${this.apiUrl}/trending/${params.toggleValue}`, this.options);
       return response.json();
     }
   })
@@ -47,7 +45,7 @@ export class MovieDataClient {
   public popularMovies = resource({
     params: () => ({ toggleValue: this.popularMovieToggle() }),
     loader: async ({ params }) => {
-      const response = await fetch(`http://localhost:3000/api/popular/${params.toggleValue}`, this.options);
+      const response = await fetch(`${this.apiUrl}/popular/${params.toggleValue}`, this.options);
       return response.json();
     }
   })
@@ -55,7 +53,7 @@ export class MovieDataClient {
   public freeToWatch = resource({
     params: () => ({ toggleValue: this.freeToWatchToggle() }),
     loader: async ({ params }) => {
-      const response = await fetch(`http://localhost:3000/api/free/${params.toggleValue}`, this.options);
+      const response = await fetch(`${this.apiUrl}/free/${params.toggleValue}`, this.options);
       return response.json();
     }
   })
